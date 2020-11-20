0d74eeec1a5789df84baf64f328d0bb50c8efee8
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-11-20 15:53:22 +0800

    test

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/"></Route>
+        {/* home page */}
+        <Route path="login"></Route>
+        {/* login page */}
+        <Route path="/register"></Route>
+        {/* register page */}
+        <Route path="/dashboard/cust/:id"></Route>
+        {/* customer dashboard */}
+        <Route path="/dashboard/ws/:id"></Route>
+        {/* workshop dashboard */}
+        <Route></Route>
+      </Switch>
+    </Router>
   );
 }
 
