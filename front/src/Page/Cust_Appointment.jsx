cc6f7684c817c36e381ef3f26918bab902db08b3
khairulsyazwan <khairulsyazwanbm@gmail.com>
2020-11-24 19:04:47 +0800

    some styling changes

--- front/src/Page/Cust_Appointment.jsx
+++ front/src/Page/Cust_Appointment.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Card, Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
 
 function Cust_Appointment() {
   const [app, setApp] = useState();
@@ -24,31 +24,50 @@ function Cust_Appointment() {
   }, []);
 
   return (
-    <div>
+    <>
+      <Navbar bg="primary" variant="light">
+        <Navbar.Brand>Workshop Buddy</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link>Features</Nav.Link>
+        </Nav>
+      </Navbar>
       <div>
-        <Container>
+        <Container className="my-2">
           <h1>Appointment Details</h1>
           {app && app.isAcknowledged ? (
-            <span class="badge badge-pill badge-success">Acknowledged</span>
+            <span class="badge badge-pill badge-success my-2">
+              Acknowledged
+            </span>
           ) : (
-            <span class="badge badge-pill badge-danger">
+            <span class="badge badge-pill badge-danger my-2">
               Pending Acknowledgement
             </span>
           )}
 
           <Row>
             <Col>
-              <h3>{app && app.date}</h3>
-              <h3>{app && app.vehicle.vehicleNumber}</h3>
-              <h3>{app && app.vehicle.make + " " + app.vehicle.model}</h3>
-              <h3>{app && app.work}</h3>
-              <h3>{app && app.workshop.name}</h3>
-              <h3>{app && app.workshop.address}</h3>
+              <Card>
+                <Card.Header>
+                  <h3>{app && app.date}</h3>
+                </Card.Header>
+                <Card.Body>
+                  <h5>{app && app.vehicle.vehicleNumber}</h5>
+                  <h5>{app && app.vehicle.make + " " + app.vehicle.model}</h5>
+                  <h5>{app && app.work}</h5>
+                </Card.Body>
+                <Card.Footer>
+                  <h5>{app && app.workshop.name}</h5>
+                  <h5>{app && app.workshop.address}</h5>
+                </Card.Footer>
+              </Card>
             </Col>
           </Row>
         </Container>
       </div>
-    </div>
+    </>
   );
 }
 
